4a8909a304beb06b2ff454b2c459150b1152d252
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2022-12-20 21:37:45 +0900

    feat: front file update

---
+++ front/src/App.js
@@ -0,0 +1,19 @@
+/* eslint no-use-before-define: 0 */
+import './App.css';
+
+function App() {
+  return (
+    <div id="layout">
+      <header id="header"></header>
+      <main id="main">
+        <aside id="aside">
+          <div id="scroll"></div>
+        </aside>
+        <section id="section"></section>
+      </main>
+      <footer id="footer"></footer>
+    </div>
+  );
+}
+
+export default App;
