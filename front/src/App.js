90d61b152203915dda7515130cd3698e23ec446c
jjeonge97 <cowguswjd@gmail.com>
2022-12-21 14:07:17 +0900

    feat:header component

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,23 @@
 /* eslint no-use-before-define: 0 */
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div id="layout">
-      <header id="header"></header>
+      <header id="header">
+        <Header />
+      </header>
       <main id="main">
         <aside id="aside">
           <div id="scroll"></div>
         </aside>
         <section id="section"></section>
       </main>
-      <footer id="footer"></footer>
+      <footer id="footer">
+        <Footer />
+      </footer>
     </div>
   );
 }
