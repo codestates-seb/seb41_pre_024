214878d9cab2d7b1f7d040c9b3b6709880b059af
“Pikadev1771” <pikadev1771@gmail.com>
2022-12-21 11:08:42 +0900

    feat: questions page router

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,29 @@
 /* eslint no-use-before-define: 0 */
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import QandA from './components/QandA';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div id="layout">
-      <header id="header"></header>
-      <main id="main">
-        <aside id="aside">
-          <div id="scroll"></div>
-        </aside>
-        <section id="section"></section>
-      </main>
-      <footer id="footer">
-        <Footer />
-      </footer>
-    </div>
+    <BrowserRouter>
+      <div id="layout">
+        <header id="header"></header>
+        <main id="main">
+          <aside id="aside">
+            <div id="scroll"></div>
+          </aside>
+          <section id="section">
+            <Routes>
+              <Route path="/" />
+              <Route path="/questions" element={<QandA />} />
+            </Routes>
+          </section>
+        </main>
+        <Footer id="footer" />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ front/src/components/QandA.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function QandA() {
+  return <h1>QandA</h1>;
+}
