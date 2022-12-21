2aaf2ce6dc8eef6d21aa461fd560e53e6d4d1c01
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2022-12-21 15:46:39 +0900

    feat:reset

--- front/src/components/Questions.js
+++ front/src/components/Questions.js
@@ -1,85 +1,8 @@
 import styled from "styled-components";
 import useState from "react";
-import Tap from "./Tap"
-
-const Layout = styled.div`
-  display: flex;
-
-  .flex_3 {
-    flex: 5;
-    
-  }
-  .flex_1 {
-    flex: 2;
-    margin-left:20px;
-    background-color: rgba(255, 255, 0, 0.5);
-  }
-`;
-
-const Header = styled.header`
-  .title {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding-bottom: 30px;
-  }
-
-  .tap {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 12px;
-  }
-
-
-  .crate{
-    background-color: rgb(21,126,255);
-    padding:10px;
-    border-radius:5px;
-    border-top: 1px inset white;
-    a{
-        color: white; 
-    }
-  }
-
-  .tap_ul {
-    display: flex;
-
-  }
-`;
 
 
 
 export const Questios = () => {
-  return (
-    <Layout>
-      <div className="flex_3">
-        <Header>
-          <div className="title">
-            <div className="">
-              {/* 기본제목 & 검색시 제목 */}
-              <h2>All Questions</h2>
-            </div>
-            <div className="crate">
-              {/* 클릭시 질문작성 페이지 (비로그인시 로그인창으로) */}
-              <a href="#">Ask Question</a>
-            </div>
-          </div>
-          <div className="tap">
-            <div>
-              {/* 리스트 목록개수 */}
-              <h3>257 questions with bounties</h3>
-            </div>
-            {/* 탭 버튼 */}
-            <div>
-            </div>
-          </div>
-        </Header>
-
-        <main>리스트부분</main>
-        <footer>페이지 네이션</footer>
-      </div>
-      <aside className="flex_1">사이드부분</aside>
-    </Layout>
-  );
+  return (<div>reset</div>);
 };
