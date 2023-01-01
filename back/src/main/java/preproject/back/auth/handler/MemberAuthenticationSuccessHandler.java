99538087b8fe9e8b95a2af5a5831f314ef63502d
Sangwonee <111265742+Sangwonee@users.noreply.github.com>
2023-01-02 03:53:37 +0900

    feat: JWT, Login response (#67)
    Co-authored-by: 윤상원 <yunsang-won@yunsang-won-ui-MacBookPro.local>

--- back/src/main/java/preproject/back/auth/handler/MemberAuthenticationSuccessHandler.java
+++ back/src/main/java/preproject/back/auth/handler/MemberAuthenticationSuccessHandler.java
@@ -1,20 +1,42 @@
 package preproject.back.auth.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import preproject.back.Member.Entity.Member;
+import preproject.back.Member.mapper.MemberMapper;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collection;
 
 @Slf4j
 public class MemberAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    private ObjectMapper objectMapper = new ObjectMapper();
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
-                                        Authentication authentication) throws IOException {
+                                        Authentication authentication) throws IOException , ServletException {
         // 인증 성공 후, 로그를 기록하거나 사용자 정보를 response로 전송하는 등의 추가 작업을 할 수 있다.
+        //Authentication 객체에 사용자 정보를 얻은 후, HttpServletResponse로 출력 스트림을 생성하여 response를 전송할 수 있다
+
+        String memberEmail = authentication.getName();
+        Collection<?> roles = authentication.getAuthorities();
+        System.out.println("Email: " + memberEmail);
+        System.out.println("Authority: "+ roles);
+
+        Member member = (Member) authentication.getPrincipal();
+//        response.setStatus(HttpStatus.OK.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        objectMapper.writeValue(response.getWriter(), member);
+
+
+
         log.info("# Authenticated successfully!");
     }
 }
